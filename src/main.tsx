6a99519d20950904a621adf6eb9c299aa0a44ece
AndreyPokotilo <dpon444@gmail.com>
2025-11-01 17:20:07 +0100

    corect name

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="/e-fix-and-go">
+    <BrowserRouter basename="/my_portfolio">
       <App />
     </BrowserRouter>
   </StrictMode>
